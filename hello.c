a4f2a9ff1b9acc64e67d160a56772384a2c659f3
Stefan Hundhammer <Stefan.Hundhammer@gmx.de>
2020-04-23 13:54:35 +0200

    Simple program to test the gettext tools

---
+++ hello.c
@@ -0,0 +1,41 @@
+/**
+ * Simple program to test the gettext tools
+ *
+ * Build with
+ *
+ *     make hello
+ *
+ * (yes, this works without a Makefile)
+ *
+ * Author:  Stefan Hundhammer <shundhammer@suse.de>
+ * License: GPL V2
+ **/
+
+#include <stdio.h>
+#include <libintl.h>
+#include <locale.h>
+
+#define TEXTDOMAIN "hello"
+
+#define _(MSG)  dgettext( TEXTDOMAIN, (MSG) )
+
+
+int main( int argc, char **argv )
+{
+    setlocale( LC_ALL, "" );
+    bindtextdomain( TEXTDOMAIN, "/usr/share/locale" );
+    textdomain( TEXTDOMAIN );
+    
+    printf( _( "Hello, world!\n" ) );
+
+    printf( _( "printf-like parameters %s and %s\n" ), "foo", "bar" );
+    printf( _( "Numbered printf parameters %2$s and %1$s\n" ), "foo", "bar" );
+
+    
+    // Just emulating YaST / Ruby style for the gettext tools;
+    // this does not do anyting useful with plain C/C++:
+    
+    puts( _( "sformat-like message with parameters %1 and %2" ) );
+    puts( _( "Named parameters %{foo} and %{bar}" ) );
+    puts( _( "Named parameters %<foo> and %<bar>" ) );
+}
